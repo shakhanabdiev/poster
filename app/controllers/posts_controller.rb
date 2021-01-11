a60928c2dc8ed0d88b295c8ca3cba06ad2959b36
Shakhan <st091451@student.spbu.ru>
2021-01-12 00:54:27 +0500

    update frontend with custom bootstrap and add validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,43 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end
-    def new
 
+    def new
+        @post = Post.new
     end
+
+    def destroy
+        @post = Post.find(params[:id])
+
+        @post.destroy
+        redirect_to posts_path
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
     def show
         @post = Post.find(params[:id])
     end
+    
     def create
         # render plain: params[:post].inspect
         @post = Post.new(post_params)
-        @post.save
+        if @post.save
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
 
-        redirect_to @post
+    def update
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to @post
+        else
+            render 'edit'
+        end
     end
 
     private
