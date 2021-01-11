0ea2019f6da5e87d78b835b85e7d5c0d49c463c7
Shakhan <st091451@student.spbu.ru>
2021-01-12 01:20:00 +0500

    add to routes commets path dinamicly bind to post

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root "posts#index", as: "home"
   get "about" => 'pages#about', as: "about"
   get "contact" => 'pages#contact'
-  resources "posts"
+  resources "posts" do 
+    resources "comments"
+  end
 end
